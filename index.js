417c5ba68d010b25709a484382f49ea74cabfd20
Mizael Paredes <mizael.paredes@externos.bci.cl>
2020-05-07 22:54:16 -0400

    Maquetación estandar de html y css

---
+++ index.js
@@ -0,0 +1,22 @@
+const url = "https://covid19.mathdro.id/api/countries/chile";
+const request = document.getElementById("search");
+
+request.addEventListener("click", () => {
+  alert("prueba");
+});
+// const result = fetch(url);
+// result
+//   .then(data => {
+//     return data.json();
+//   })
+//   .then(({ confirmed }) => {
+//     // let count = data.confirmed
+//     renderizar(confirmed.value);
+//   });
+
+// const renderizar = number => {
+//   if (number)
+//     document.body.innerText = `La cantidad de casos confirmados en Chile es de: ${number}`;
+// };
+
+// renderizar();
