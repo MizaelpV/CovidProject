39912a0b121564eff6ffd3becd58d7324d3979ea
Mizael Paredes <mizael.paredes@externos.bci.cl>
2020-05-10 23:54:26 -0400

    Se crea logica de data general

--- index.js
+++ index.js
@@ -1,23 +1,83 @@
-const url = "https://covid19.mathdro.id/api/countries/chile";
-const request = document.getElementById("search");
+const confirmado = document.getElementById("confirmed");
+const muerte = document.getElementById("deaths");
+const recuperado = document.getElementById("recovery");
+const hero = document.getElementById("hero");
 
-const result = fetch(url);
-result
-  .then(data => {
-    return data.json();
-  })
-  .then(({ confirmed }) => {
-    //  let count = data.confirmed
-    renderizar(confirmed.value);
-  });
+//functions to request Data
+function recoveredData() {
+  const url = "https://covid19.mathdro.id/api/";
+  try {
+    fetch(url)
+      .then(data => {
+        return data.json();
+      })
+      .then(({ recovered }) => {
+        render(recuperado, recovered.value);
+      });
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-const renderizar = number => {
-  console.log(`La cantidad de casos confirmados es de: ${number}`);
-};
+recoveredData();
 
-request.addEventListener("click", () => {
-  alert("prueba");
-});
-renderizar();
+function confirmedData() {
+  const url = "https://covid19.mathdro.id/api/";
+  try {
+    fetch(url)
+      .then(data => {
+        return data.json();
+      })
+      .then(({ confirmed }) => {
+        render(confirmado, confirmed.value);
+      });
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-console.log("Probando ");
+confirmedData();
+
+function deathsData() {
+  const url = "https://covid19.mathdro.id/api/";
+  try {
+    fetch(url)
+      .then(data => {
+        return data.json();
+      })
+      .then(({ deaths }) => {
+        render(muerte, deaths.value);
+      });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function render(type, value) {
+  return (type.innerText = `La cantidad de ${type.id} es de:\n${value}\n a nivel mundial`);
+}
+
+deathsData();
+
+function getWeekDay(date) {
+  let weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = date.getDay();
+  return weekdays[day];
+}
+
+let date = new Date();
+let day = getWeekDay(date);
+let numberDay = date.getDate();
+let month = date.getMonth();
+let year = date.getFullYear();
+
+hero.innerText = `Today is ${day}, ${month}/${numberDay}/${year}`;
+console.log(`Today is ${day}, ${month}/${numberDay}/${year}`);
