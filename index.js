18c7bc1fdb5285bd9168e19dc39ee55d54950f9a
Mizael Paredes <mizael.paredes@externos.bci.cl>
2020-05-13 08:56:37 -0400

    Creacion de logica para buscar por paises y mejoras en maquetacion y diseño

--- index.js
+++ index.js
@@ -1,83 +1,157 @@
-const confirmado = document.getElementById("confirmed");
-const muerte = document.getElementById("deaths");
-const recuperado = document.getElementById("recovery");
+const confirmado = document.getElementById("confirmados");
+const muerte = document.getElementById("fallecidos");
+const recuperado = document.getElementById("recuperados");
 const hero = document.getElementById("hero");
+const searchCountry = document.getElementById("searchCountry");
+const search = document.getElementById("search");
+const url = "https://covid19.mathdro.id/api/";
+
+let formatNumber = {
+  separador: ".",
+  sepDecimal: ",",
+  formatear: function(num) {
+    num += "";
+    let splitStr = num.split(".");
+    let splitLeft = splitStr[0];
+    let splitRight = splitStr.length > 1 ? this.sepDecimal + splitStr[1] : "";
+    let regx = /(\d+)(\d{3})/;
+    while (regx.test(splitLeft)) {
+      splitLeft = splitLeft.replace(regx, "$1" + this.separador + "$2");
+    }
+    return this.simbol + splitLeft + splitRight;
+  },
+  new: function(num, simbol) {
+    this.simbol = simbol || "";
+    return this.formatear(num);
+  }
+};
 
 //functions to request Data
 function recoveredData() {
-  const url = "https://covid19.mathdro.id/api/";
   try {
     fetch(url)
       .then(data => {
         return data.json();
       })
       .then(({ recovered }) => {
-        render(recuperado, recovered.value);
+        let r = formatNumber.new(recovered.value);
+        render(recuperado, r);
       });
   } catch (error) {
-    console.error(error);
+    return console.error(error);
   }
 }
 
 recoveredData();
 
 function confirmedData() {
-  const url = "https://covid19.mathdro.id/api/";
   try {
     fetch(url)
       .then(data => {
         return data.json();
       })
       .then(({ confirmed }) => {
-        render(confirmado, confirmed.value);
+        let c = formatNumber.new(confirmed.value);
+        render(confirmado, c);
       });
   } catch (error) {
-    console.error(error);
+    return console.error(error);
   }
 }
 
 confirmedData();
 
 function deathsData() {
-  const url = "https://covid19.mathdro.id/api/";
   try {
     fetch(url)
       .then(data => {
         return data.json();
       })
       .then(({ deaths }) => {
-        render(muerte, deaths.value);
+        let d = formatNumber.new(deaths.value);
+        render(muerte, d);
       });
   } catch (error) {
-    console.error(error);
+    return console.error(error);
   }
 }
 
 function render(type, value) {
-  return (type.innerText = `La cantidad de ${type.id} es de:\n${value}\n a nivel mundial`);
+  return (type.innerText = `La cantidad de casos\n ${type.id} es de\n${value} personas\n a nivel mundial`);
 }
 
 deathsData();
 
 function getWeekDay(date) {
   let weekdays = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
+    "Domingo",
+    "Lunes",
+    "Martes",
+    "Miercoles",
+    "Jueves",
+    "Viernes",
+    "Sábado"
   ];
   let day = date.getDay();
   return weekdays[day];
 }
 
-let date = new Date();
-let day = getWeekDay(date);
-let numberDay = date.getDate();
-let month = date.getMonth();
-let year = date.getFullYear();
+function getMonthString(date) {
+  let months = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre"
+  ];
+  let month = date.getMonth();
+  return months[month];
+}
 
-hero.innerText = `Today is ${day}, ${month}/${numberDay}/${year}`;
-console.log(`Today is ${day}, ${month}/${numberDay}/${year}`);
+function setDateNow() {
+  let date = new Date();
+  let day = getWeekDay(date);
+  let numberDay = date.getDate();
+  let month = getMonthString(date);
+  let year = date.getFullYear();
+  hero.innerText = `Hoy es ${day} ${numberDay} de ${month} de ${year}`;
+}
+
+setDateNow();
+
+const urlCountries = "https://covid19.mathdro.id/api/countries";
+const fetchCountryConfirmed = (async = country => {
+  try {
+    fetch(`${urlCountries}/${country}`).then(async data => {
+      const { confirmed } = await data.json();
+      let confirm = formatNumber.new(confirmed.value);
+      let modified =
+        country.substring(0, 1).toUpperCase() + country.substring(1);
+      console.log(
+        `La cantidad de casos confirmados en ${modified} es de ${confirm} personas`
+      );
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+const renderCountry = event => {
+  event.preventDefault();
+  if (searchCountry.value) {
+    let country = searchCountry.value.toLowerCase();
+    fetchCountryConfirmed(country);
+    searchCountry.value = "";
+  } else {
+    console.log("No puede estar vacío");
+  }
+};
+
+search.addEventListener("click", renderCountry);
